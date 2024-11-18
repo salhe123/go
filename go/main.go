099cf33ad7b5b38be1a199613b8ad710420d402b
salhe123 <salheseid92@gmail.com>
2024-11-18 11:40:36 +0300

    server check

--- go/main.go
+++ go/main.go
@@ -18,7 +18,7 @@ func main() {
 	}
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "3000"
+		port = "5000"
 	}
 	router := mux.NewRouter()
 	router.HandleFunc("/signup", handlers.SignupHandler).Methods("POST")
